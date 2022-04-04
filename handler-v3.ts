2e479e3b8c37567733af8765f85933348033a322
James Lockman <jlockman@adobe.com>
2022-04-04 15:07:32 -0400

    Add V3 endpoints for Schedule and Match results
    Adding schedule, match results, and scores as V3 endpoints.

--- handler-v3.ts
+++ handler-v3.ts
@@ -13,4 +13,21 @@ const GetEventsV3: Handler = async (event: APIGatewayEvent) => {
   return await GetDataFromFIRSTAndReturn(`${event.pathParameters.year}/events/`, apiVersion);
 };
 
-export { GetEventAwards, GetEventsV3 }
+// noinspection JSUnusedGlobalSymbols
+const GetScheduleV3: Handler = async (event: APIGatewayEvent) => {
+  return await GetDataFromFIRSTAndReturn(event.pathParameters.year + '/schedule/' +
+      event.pathParameters.eventCode + '/' + event.pathParameters.tournamentLevel, apiVersion);
+};
+
+// noinspection JSUnusedGlobalSymbols
+const GetMatchesV3: Handler = async (event: APIGatewayEvent) => {
+  return await GetDataFromFIRSTAndReturn(event.pathParameters.year + '/matches/' +
+      event.pathParameters.eventCode + '/' + event.pathParameters.tournamentLevel, apiVersion);
+};
+
+// noinspection JSUnusedGlobalSymbols
+const GetEventScoresV3: Handler = async (event: APIGatewayEvent) => {
+  return await GetDataFromFIRSTAndReturn(`${event.pathParameters.year}/scores/${event.pathParameters.eventCode}/${event.pathParameters.tournamentLevel}/${event.pathParameters.start}/${event.pathParameters.end}`, apiVersion);
+};
+
+export { GetEventAwards, GetEventsV3, GetScheduleV3, GetMatchesV3, GetEventScoresV3 }
