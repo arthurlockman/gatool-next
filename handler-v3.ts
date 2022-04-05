6e8ea4eeac3d8338bf8c010aa9f47619aa8135f3
James Lockman <jlockman@adobe.com>
2022-04-05 13:33:13 -0400

    Updating V3 handler imports

--- handler-v3.ts
+++ handler-v3.ts
@@ -1,7 +1,7 @@
 import { APIGatewayEvent, Handler } from 'aws-lambda';
 import _ = require('lodash');
 import { APIGatewayResponse, ResponseWithHeaders } from './types';
-import { CreateResponseJson, GetDataFromFIRST, GetDataFromFIRSTAndReturn } from './utils/utils';
+import { CreateResponseJson, GetDataFromFIRST, GetDataFromFIRSTAndReturn, ReturnJsonWithCode } from './utils/utils';
 
 const apiVersion = 'v3.0';
 
@@ -69,29 +69,29 @@ const GetDistrictRankingsV3: Handler = async (event: APIGatewayEvent) => {
   const query = [];
   query.push(`districtCode=${event.pathParameters.districtCode}`);
   if (event.queryStringParameters) {
-      const top = event.queryStringParameters.top;
-      if (top) {
-          query.push(`top=${top}`);
-      }
+    const top = event.queryStringParameters.top;
+    if (top) {
+      query.push(`top=${top}`);
+    }
   }
   const rankingData = await GetDataFromFIRST(`${event.pathParameters.year}/rankings/district?${query.join('&')}&page=1`, apiVersion);
   if (rankingData.body.statusCode) {
-      return ReturnJsonWithCode(rankingData.body.statusCode, rankingData.body.message);
+    return ReturnJsonWithCode(rankingData.body.statusCode, rankingData.body.message);
   }
   if (rankingData.body.pageTotal === 1) {
-      return ReturnJsonWithCode(200, rankingData.body, rankingData.headers);
+    return ReturnJsonWithCode(200, rankingData.body, rankingData.headers);
   } else {
-      const promises: Promise<any>[] = [];
-      for (let i = 2; i <= rankingData.body.pageTotal; i++) {
-          promises.push(GetDataFromFIRST(`${event.pathParameters.year}/rankings/district?${query.join('&')}&page=${i}`, apiVersion));
-      }
-      const allRankData = await Promise.all(promises);
-      allRankData.map(districtRank => {
-          rankingData.body.rankingCountPage += districtRank.body.rankingCountPage;
-          rankingData.body.districtRanks = rankingData.body.districtRanks.concat(districtRank.body.districtRanks);
-      });
-      rankingData.body.pageTotal = 1;
-      return ReturnJsonWithCode(200, rankingData.body, rankingData.headers);
+    const promises: Promise<any>[] = [];
+    for (let i = 2; i <= rankingData.body.pageTotal; i++) {
+      promises.push(GetDataFromFIRST(`${event.pathParameters.year}/rankings/district?${query.join('&')}&page=${i}`, apiVersion));
+    }
+    const allRankData = await Promise.all(promises);
+    allRankData.map(districtRank => {
+      rankingData.body.rankingCountPage += districtRank.body.rankingCountPage;
+      rankingData.body.districtRanks = rankingData.body.districtRanks.concat(districtRank.body.districtRanks);
+    });
+    rankingData.body.pageTotal = 1;
+    return ReturnJsonWithCode(200, rankingData.body, rankingData.headers);
   }
 };
 
